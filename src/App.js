4d773f8698fb1633635e32d627fd5dccb5a84d00
Ramneek Singh <48586823+ramneek008@users.noreply.github.com>
2020-06-30 16:59:06 +0530

    setup layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import './App.css';
+
 import Icon from './components/Icon';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { Card, CardBody, Container, Button, Col, Row } from "reactstrap";
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+const itemArray = new Array(9).fill("empty");
+
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <Icon />
-      </header>
-    </div>
+    <Container className="p-5">
+      <ToastContainer position="bottom-center" />
+      <Row>
+        <Col md={6} className="offset-md-3">
+          <div className="grid">
+            {itemArray.map((item, index) => (
+              <Card>
+                <CardBody className="box">
+                  <Icon name={item} />
+                </CardBody>
+              </Card>
+            ))}
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 

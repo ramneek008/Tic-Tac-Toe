0abe7c41876fe22ee36d9e3baa467cb4221bae99
Ramneek Singh <48586823+ramneek008@users.noreply.github.com>
2020-07-01 17:19:28 +0530

    css updated

--- src/App.js
+++ src/App.js
@@ -59,17 +59,23 @@ const App = () => {
     {
       setWinMessage(itemArray[1] + " wins");
     }
-    if(itemArray[2] !== "empty" && itemArray[2] === itemArray[5] && itemArray[5] === itemArray[8])
+    else if(itemArray[2] !== "empty" && itemArray[2] === itemArray[5] && itemArray[5] === itemArray[8])
     {
       setWinMessage(itemArray[2] + " wins");
     }
-    if(itemArray[0] !== "empty" && itemArray[0] === itemArray[4] && itemArray[4] === itemArray[8])
+    else if(itemArray[0] !== "empty" && itemArray[0] === itemArray[4] && itemArray[4] === itemArray[8])
     {
       setWinMessage(itemArray[0] + " wins");
     }
-    if(itemArray[2] !== "empty" && itemArray[2] === itemArray[4] && itemArray[4] === itemArray[6])
+    else if(itemArray[2] !== "empty" && itemArray[2] === itemArray[4] && itemArray[4] === itemArray[6])
     {
       setWinMessage(itemArray[2] + " wins");
+    }
+    else if(itemArray[0] !=="empty" && itemArray[1] !=="empty" && itemArray[2] !=="empty" && 
+            itemArray[3] !=="empty" && itemArray[4] !=="empty" && itemArray[5] !=="empty" && 
+            itemArray[6] !=="empty" && itemArray[7] !=="empty" && itemArray[8] !=="empty")
+    {
+      setWinMessage("Match Tied");
     }
   }
 
@@ -78,21 +84,26 @@ const App = () => {
       <ToastContainer position="bottom-center" />
       <Row>
         <Col md={6} className="offset-md-3">
+          <h1 className="title text-center text-uppercase text-white mb-4">Tic-Tac-Toe</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={6} className="offset-md-3">
           {winMessage ? (
-            <div className="mb-2 mt-2">
-              <h1 className="text-success text-uppercase text-center">
+            <div className="mb-2 mt-4">
+              <h2 className="text-success text-uppercase text-center">
                 {winMessage}
-              </h1>
+              </h2>
               <Button color="success" block onClick={reloadGame}>Reload Game</Button>
             </div>
           ) : (
-            <h1 class="text-center text-warning">
-              {isCross ? "Cross" : "Circle"} turn
-            </h1>
+            <h2 className="text-center text-warning mb-4 mt-4">
+              {isCross ? "Cross" : "Circle"}'s turn
+            </h2>
           )}
           <div className="grid">
             {itemArray.map((item, index) => (
-              <Card onClick={() => changeItem(index)}>
+              <Card color="warning" onClick={() => changeItem(index)}>
                 <CardBody className="box">
                   <Icon name={item} />
                 </CardBody>
